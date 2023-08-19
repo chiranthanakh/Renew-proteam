b1a299d730a58f7fe4856839671812879c51200b
Prinsh <prinshsharma567@gmail.com>
2023-08-19 23:17:44 +0530

    19

---
+++ app/src/main/java/com/proteam/renew/views/ObservationTrackingActivity.kt
@@ -0,0 +1,12 @@
+package com.proteam.renew.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.proteam.renew.R
+
+class ObservationTrackingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_observation_tracking)
+    }
+}
