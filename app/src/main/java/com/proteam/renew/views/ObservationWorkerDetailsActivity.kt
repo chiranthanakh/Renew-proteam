d426ca61cd29d752971794b52525391f7cdb31a7
Prinsh <prinshsharma567@gmail.com>
2023-08-20 14:09:10 +0530

    20

---
+++ app/src/main/java/com/proteam/renew/views/ObservationWorkerDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.proteam.renew.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.proteam.renew.R
+
+class ObservationWorkerDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_observation_worker_details)
+    }
+}
